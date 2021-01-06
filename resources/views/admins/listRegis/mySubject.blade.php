dcae99c2f55217733a46e4186df597b93d837879
pttuoi99 <tuoipt.99@gmail.com>
2021-01-06 23:26:23 +0700

    danh sach dang ki khoa hoc

--- resources/views/admins/listRegis/mySubject.blade.php
+++ resources/views/admins/listRegis/mySubject.blade.php
@@ -7,6 +7,7 @@
                         <thead>
                             <tr>
                                 <th>Danh mục khóa học</th>
+                                <th>Ảnh minh họa</th>
                                 <th>Tên khóa học</th>
                                 <th>Ngày bắt đầu</th>
                                 <th>Ngày kết thúc</th>
@@ -15,6 +16,7 @@
                         <tbody>
                             <tr>
                                 <td>{{ $subjects->nameCourse }}</td>
+                                <td><img width="100px" src="{{ pare_url_file($subjects->image) }}"></td>
                                 <td>{{ $subjects->name }}</td>
                                 <td>{{ $subjects->date_start }}</td>
                                 <td>{{ $subjects->date_end }}</td>
